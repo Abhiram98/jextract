2fae5d2111000ca7fa56ee431fd54b2c71f2db20
Abhiram98 <bellur.abhiram@gmail.com>
2024-03-05 18:28:51 -0700

    refactoring to read from environment variables

--- README.md
+++ README.md
@@ -3,6 +3,20 @@ JExtract
 
 JExtract is a Extract Method Refactoring recommendation tool
 
+# Building the plugin
+Follow the first two steps [here](https://medium.com/@ravi_theja/creating-your-first-eclipse-plugin-9b1b5ba33b58) 
+to setup your eclipse environment.
+
+# Running Custom Evaluation
+1. Set the following environment variable 
+   1. `PROJECT_NAME`
+   2. `PROJECT_PATH`
+   3. `DATA_FILE` - file containing the list of refactorings in the oracle. Use the same schema as [this](example-data.json) file.
+   4. `OUT_DIRECTORY` - results from running jextract go here.
+2. Launch Jextract 
+3. Right click on project:
+
+   `Jextract -> Find Extract Method Opportuniies ->`
 # CLI
 
 Java version: java 11
--- br.ufmg.dcc.labsoft.jextract/src/br/ufmg/dcc/labsoft/jextract/ui/AbstractProjectMenuAction.java
+++ br.ufmg.dcc.labsoft.jextract/src/br/ufmg/dcc/labsoft/jextract/ui/AbstractProjectMenuAction.java
@@ -1,7 +1,7 @@
 package br.ufmg.dcc.labsoft.jextract.ui;
 
 import java.io.*;
-import java.lang.reflect.Method;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,11 +12,8 @@ import com.google.gson.JsonParser;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jdt.core.IMethod;
-import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.window.Window;
-import org.eclipse.swt.widgets.Display;
 
 import br.ufmg.dcc.labsoft.jextract.generation.EmrGenerator;
 import br.ufmg.dcc.labsoft.jextract.generation.Settings;
@@ -48,183 +45,183 @@ public abstract class AbstractProjectMenuAction<T> extends ObjectMenuAction<T> {
 				@Override
 				protected void doWorkIteration(int i, IProgressMonitor monitor) throws Exception {
 
-					JsonParser parser = new JsonParser();
-					String projectName = "CoreNLP";
-					String subproject = "";
-//					String projectName = "intellij-community";
-//					String subproject = "platform";
-					String jsonFileName =
-							String.format("/Users/abhiram/Documents/JetGPT/extended_corpus/%s-data.json", projectName);
-					String outDir = "/Users/abhiram/Documents/JetGPT/extended_corpus/JExtractOut/";
-					JsonArray data = (JsonArray) parser.parse(new FileReader(jsonFileName));
-//					String projectPath = project.getRawLocation().toString();
-					String projectPath = String.format("/Users/abhiram/Documents/JetGPT/extended_corpus/projects/%s",projectName);
-					String basePath = "/Users/abhiram/Documents/JetGPT/extended_corpus/projects";
-					Integer count = 0;
-					Integer limit = 2000;
-
-
-					JsonArray completed = (JsonArray) parser.parse(
-							new FileReader(
-								String.format("/Users/abhiram/Documents/JetGPT/extended_corpus/%s-completed.json",
-										projectName)));
-					List<Integer> completedList = new ArrayList<Integer>();
-					for (Object c : completed) {
-						completedList.add(Integer.parseInt(c.toString()));
-					}
-
-					for (Object obj : data) {
-						System.out.println(count+" Complteted.");
-//						if (completedList.contains(count)) {
-//							System.out.println(count+ " done");
-//							count +=1;
-//							continue;
-//						}
-						if (count > limit){
-							break;
-						}
-						JsonObject ref = (JsonObject) obj;
-						String pname = ref.get("projectName").toString().replaceAll("\"", "");
-						String sha = ref.get("sha").toString().replaceAll("\"", "");
-						String fileName = ref.get("filename").toString().replaceAll("\"", "");
-						String functionName = ref.get("functionName").toString().replaceAll("\"", "");
-						System.out.println(pname);
-						System.out.println(sha);
-						System.out.println(fileName);
-						System.out.println(functionName);
-
-						if (subproject.equals("") || fileName.startsWith(subproject+"/")){
-							System.out.println("Found subproject");
-						}
-						else{
-							count+=1;
-							continue;
-						}
-
-//						project.close(null);
-						// Change git to Sha
-						Runtime rt = Runtime.getRuntime();
-						String[] gitChange = {"git", "-C", projectPath, "checkout", "-f", sha};
-						rt.exec(gitChange).waitFor();
-
-						// Refresh project.
-//						BufferedReader stdInput = new BufferedReader(new
-//								InputStreamReader(rt.exec("pwd").getInputStream()));
-//						System.out.println("Here is the standard output of the command:\n");
-//						String s = null;
-//						while ((s = stdInput.readLine()) != null) {
-//							System.out.println(s);
-//						}
-
-						String p2;
-						if (subproject.equals("")){
-							p2="";
-						}else{
-							p2=subproject+"/";
-						}
-
-						String [] cpProject = {"cp",
-								String.format("%s/.%sproject", basePath, projectName),
-								String.format("%s/%s.project", projectPath, p2)};
-						File f = new File(String.format("%s/%s.project", projectPath, p2));
-						if(!f.exists()) {
-							// do something
-							rt.exec(cpProject).waitFor();
-						}
-						String [] cpClasspath = {"cp", String.format("%s/.%sclasspath", basePath, projectName),
-								String.format("%s/%s.classpath", projectPath, p2)};
-						File f2 = new File(String.format("%s/%s.classpath", projectPath, p2));
-						if(!f2.exists()) {
-							// do something
-							rt.exec(cpClasspath).waitFor();
-						}
-//						project.open(null);
-						String outFile = outDir+projectName+"-"+count.toString();
-
-						try {
-//							project.refreshLocal(IResource.DEPTH_INFINITE, null);
-//							project.refreshLocal(1, null);
-							String splitSrc = fileName.split("/src")[0];
-							String srcFolder = String.format("%s/src", splitSrc);
-							project.refreshLocal(IResource.DEPTH_INFINITE, null);
-						}catch (Exception e){
-							System.out.println("Couldn't refresh project");
-							PrintWriter writer = new PrintWriter(outFile, "UTF-8");
-							writer.println("Failed to refresh project.");
-							writer.close();
-							continue;
-						}
-
-						String resultsTxt = "";
-
-						// TODO: Run Jextract
-//						project.getFile("").getClass().getMethods();
-//						JavaCore.create(project).findModule().fi;
-
-						final List<ExtractMethodRecomendation> recomendations = new ArrayList<ExtractMethodRecomendation>();
-						final EmrGenerator generator = new EmrGenerator(recomendations, settings);
-						if (subproject.equals("")) {
-							generator.setOnlyThisFile(fileName);
-						}
-						else {
-							String[] splitPaths = fileName.split(subproject + "/");
-							generator.setOnlyThisFile(splitPaths[1]);
-							generator.setOnlyThisMethodStr(functionName);
-						}
-						try {
-//							Method[] classMethods = project.getFile(fileName).getClass().getMethods();
-//							for(Method m: classMethods){
-//								if (m.getName().equals(functionName)){
-//									System.out.println("Found function!");
-//									generator.generateRecomendations(m);
-//								}
-//							}
-
-							generator.generateRecomendations(project);
-							if (recomendations.size() >0) {
-								new EmrFileExporter(recomendations, outFile).export();
-							}
-							else{
-								PrintWriter writer = new PrintWriter(outFile, "UTF-8");
-								writer.println("foundFile="+generator.foundFile);
-								writer.println("foundMethod="+generator.foundMethod);
-								writer.println("noSource="+generator.noSource);
-								writer.close();
-							}
-						} catch (Exception e){
-							System.out.println("EMR failed.");
-							File file = new File(outFile);
-							PrintStream ps = new PrintStream(file);
-							e.printStackTrace(ps);
-							ps.close();
-						}
-
-
-						count += 1;
-
-
-
-					}
-
-
-//					final List<ExtractMethodRecomendation> recomendations = new ArrayList<ExtractMethodRecomendation>();
-//					final EmrGenerator generator = new EmrGenerator(recomendations, settings);
-//					generator.generateRecomendations(project);
-//
-//					Display.getDefault().asyncExec(new Runnable() {
-//						@Override
-//                        public void run() {
-//							try {
-//								showResultView(recomendations, project, settings);
-//							} catch (Exception e) {
-//								throw new RuntimeException(e);
-//							}
-//						}
-//					});
+					analyseProjectCommits(project, settings);
 				}
 			};
 			job.schedule();
 		}
+	}
+
+	private static void analyseProjectCommits(IProject project, Settings settings) throws InterruptedException, IOException {
+		JsonParser parser = new JsonParser();
+		String projectName = System.getenv("PROJECT_NAME");
+		String projectPath = System.getenv("PROJECT_PATH");
+		String dataFile = System.getenv("DATA_FILE");
+		String outDir = System.getenv("OUT_DIRECTORY");
+		if (projectName==null || projectPath == null || dataFile == null|| outDir==null){
+			System.out.println("One or more required environment variables not set.");
+			return;
+		}
+        String basePath = new File(projectPath).getParent();
+
+		copyClasspath(basePath, projectName, projectPath);
+		copyDotProject(basePath, projectName, projectPath);
+
+//		String jsonFileName =
+//				String.format("%s/%s-data.json", dataDirectory, projectName);
+//					String jsonFileName =
+//							String.format("/Users/abhiram/Documents/JetGPT/extended_corpus/%s-data.json", projectName);
+//					String outDir = "/Users/abhiram/Documents/JetGPT/extended_corpus/JExtractOut/";
+//					String projectName = "intellij-community";
+//					String projectPath = project.getRawLocation().toString();
+//					String projectPath = String.format("/Users/abhiram/Documents/JetGPT/extended_corpus/projects/%s",projectName);
+//					String basePath = "/Users/abhiram/Documents/JetGPT/extended_corpus/projects";
+
+		JsonArray data = (JsonArray) parser.parse(new FileReader(dataFile));
+
+		Integer count = 0;
+		Integer limit = 2000; // this variable limits the number of internal Jextract calls.
+
+
+		List<Integer> completedList = getCompletedRefactorings(parser, basePath, projectName);
+
+		for (Object obj : data) {
+			System.out.println(count+" Complteted.");
+			if (completedList.contains(count)) {
+				System.out.println(count+ " done");
+				count +=1;
+				continue;
+			}
+			if (count > limit){
+				break;
+			}
+			JsonObject ref = (JsonObject) obj;
+			String pname = ref.get("projectName").toString().replaceAll("\"", "");
+			String sha = ref.get("sha").toString().replaceAll("\"", "");
+			String fileName = ref.get("filename").toString().replaceAll("\"", "");
+			String functionName = ref.get("functionName").toString().replaceAll("\"", "");
+			System.out.println(pname);
+			System.out.println(sha);
+			System.out.println(fileName);
+			System.out.println(functionName);
+
+			count+=1;
+
+//			 Change git to Sha
+			Runtime rt = checkoutGitSHA(projectPath, sha);
+
+			restoreDotProject(basePath, projectName, projectPath);
+			restoreClasspath(basePath, projectName, projectPath);
+			String outFile = outDir+projectName+"-"+count.toString();
+
+			try {
+				project.refreshLocal(IResource.DEPTH_INFINITE, null);
+			}catch (Exception e){
+				System.out.println("Couldn't refresh project");
+				PrintWriter writer = new PrintWriter(outFile, "UTF-8");
+				writer.println("Failed to refresh project.");
+				writer.close();
+				continue;
+			}
+
+
+			// Run Jextract
+			final List<ExtractMethodRecomendation> recomendations = new ArrayList<ExtractMethodRecomendation>();
+			final EmrGenerator generator = new EmrGenerator(recomendations, settings);
+			generator.setOnlyThisFile(fileName);
+			generator.setOnlyThisMethodStr(functionName);
+
+			try {
+
+				generator.generateRecomendations(project);
+				if (recomendations.size() >0) {
+					new EmrFileExporter(recomendations, outFile).export();
+				}
+				else{
+					PrintWriter writer = new PrintWriter(outFile, "UTF-8");
+					writer.println("foundFile="+generator.foundFile);
+					writer.println("foundMethod="+generator.foundMethod);
+					writer.println("noSource="+generator.noSource);
+					writer.close();
+				}
+			} catch (Exception e){
+				System.out.println("EMR failed.");
+				File file = new File(outFile);
+				PrintStream ps = new PrintStream(file);
+				e.printStackTrace(ps);
+				ps.close();
+			}
+
+
+			count += 1;
+
+		}
+	}
+
+	private static Runtime checkoutGitSHA(String projectPath, String sha) throws InterruptedException, IOException {
+		Runtime rt = Runtime.getRuntime();
+		String[] gitChange = {"git", "-C", projectPath, "checkout", "-f", sha};
+		rt.exec(gitChange).waitFor();
+		return rt;
+	}
+
+	private static List<Integer> getCompletedRefactorings(JsonParser parser, String basePath, String projectName) {
+		JsonArray completed;
+		try {
+			 completed = (JsonArray) parser.parse(
+					new FileReader(
+							String.format("%s/%s-completed.json",
+									basePath,
+									projectName)));
+		} catch (FileNotFoundException e){
+			return new ArrayList<Integer>();
+		}
+		List<Integer> completedList = new ArrayList<Integer>();
+		for (Object c : completed) {
+			completedList.add(Integer.parseInt(c.toString()));
+		}
+		return completedList;
+	}
+
+	private static void restoreClasspath(String basePath, String projectName, String projectPath) throws InterruptedException, IOException {
+		Runtime rt = Runtime.getRuntime();
+		String [] cpClasspath = {"cp", String.format("%s/.%sclasspath", basePath, projectName),
+				String.format("%s/.classpath", projectPath)};
+		File f2 = new File(String.format("%s/.classpath", projectPath));
+		if(!f2.exists()) {
+			rt.exec(cpClasspath).waitFor();
+		}
+	}
+	private static void copyClasspath(String basePath, String projectName, String projectPath) throws InterruptedException, IOException {
+		Runtime rt = Runtime.getRuntime();
+		String [] cpClasspath = {
+				"cp",
+				String.format("%s/.classpath", projectPath),
+				String.format("%s/.%sclasspath", basePath, projectName)
+		};
+		rt.exec(cpClasspath).waitFor();
+	}
+
+	private static void restoreDotProject(String basePath, String projectName, String projectPath) throws InterruptedException, IOException {
+		Runtime rt = Runtime.getRuntime();
+		String [] cpProject = {"cp",
+				String.format("%s/.%sproject", basePath, projectName),
+				String.format("%s/.project", projectPath)};
+		File f = new File(String.format("%s/.project", projectPath));
+		if(!f.exists()) {
+			// do something
+			rt.exec(cpProject).waitFor();
+		}
+	}
+
+	private static void copyDotProject(String basePath, String projectName, String projectPath) throws InterruptedException, IOException {
+		Runtime rt = Runtime.getRuntime();
+		String [] cpProject = {
+				"cp",
+				String.format("%s/.project", projectPath),
+				String.format("%s/.%sproject", basePath, projectName)
+		};
+		rt.exec(cpProject).waitFor();
 	}
 
 }
